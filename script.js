c5f644d0962f7c8853946edb36e5b17c911c3b8f
Jure Vidmar <jure.vidmar2@gmail.com>
2021-12-15 14:17:45 +0100

    Adding tags

--- script.js
+++ script.js
@@ -6,12 +6,135 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
-
+const genres = [
+    {
+       "id":28,
+       "name":"Action"
+    },
+    {
+       "id":12,
+       "name":"Adventure"
+    },
+    {
+       "id":16,
+       "name":"Animation"
+    },
+    {
+       "id":35,
+       "name":"Comedy"
+    },
+    {
+       "id":80,
+       "name":"Crime"
+    },
+    {
+       "id":99,
+       "name":"Documentary"
+    },
+    {
+       "id":18,
+       "name":"Drama"
+    },
+    {
+       "id":10751,
+       "name":"Family"
+    },
+    {
+       "id":14,
+       "name":"Fantasy"
+    },
+    {
+       "id":36,
+       "name":"History"
+    },
+    {
+       "id":27,
+       "name":"Horror"
+    },
+    {
+       "id":10402,
+       "name":"Music"
+    },
+    {
+       "id":9648,
+       "name":"Mystery"
+    },
+    {
+       "id":10749,
+       "name":"Romance"
+    },
+    {
+       "id":878,
+       "name":"Science Fiction"
+    },
+    {
+       "id":10770,
+       "name":"TV Movie"
+    },
+    {
+       "id":53,
+       "name":"Thriller"
+    },
+    {
+       "id":10752,
+       "name":"War"
+    },
+    {
+       "id":37,
+       "name":"Western"
+    }
+ ]
 
 const main = document.getElementById('main');
 const form = document.getElementById('headerForm');
 const search = document.getElementById('searchHeader');
+const tagsEl = document.getElementById('tags')
 
+var selectedGenre = []
+setGanre();
+function setGanre() {
+    tagsEl.innerHTML = '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag')
+        t.id=genre.id;
+        t.innerText= genre.name;
+        t.addEventListener('click', () => {
+            if(selectedGenre.length == 0) {
+                selectedGenre.push(genre.id)
+            }
+            else {
+                if (selectedGenre.includes(genre.id)) {
+                    selectedGenre.forEach((id, idx) => {
+                        if  (id == genre.id) {
+                            selectedGenre.splice(idx, 1)
+                        }
+                    })
+                }
+                else {
+                    selectedGenre.push(genre.id)
+                }
+            }
+            console.log(selectedGenre)
+            getMoviesPopular(API_URL+ '&with_genres='+ encodeURI(selectedGenre.join(',')))
+            highlightSelection()
+        })
+        tagsEl.append(t)
+    })
+}
+
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag'); 
+    tags.forEach(tag => {
+        tag.classList.remove('tag__highlight')
+    })
+    if (selectedGenre.length != 0){
+        selectedGenre.forEach(id => {
+            const highlightedTag = document.getElementById(id);
+            highlightedTag.classList.add('tag__highlight');
+        })
+    }
+}
 getMoviesPopular(API_URL);
 
 function getMoviesPopular(url) {
@@ -30,7 +153,7 @@ function showMovies(data) {
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-        <img src="${IMG_URL+poster_path}" alt="${title}">
+        <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580"}" alt="${title}">
         <div class="movie-info">
             <h3>${title}</h3>
             <span class="${getColor(vote_average)}">${vote_average}</span>
