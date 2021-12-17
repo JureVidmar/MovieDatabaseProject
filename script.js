0b79108efe2b6f811bc86f8aa03aa2202b6bb8d2
Jure Vidmar <jure.vidmar2@gmail.com>
2021-12-17 15:48:32 +0100

    Adding favourites and watchlist

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 //TMDB
 // dab2c8f66df561816f3de5a1bdb51d3a
+// {"success":true,"guest_session_id":"a2aaf32cb299ea85bf585b4ca65ec0ee","expires_at":"2021-12-18 08:49:05 UTC"}
+// {"success":true,"expires_at":"2021-12-17 10:04:32 UTC","request_token":"13dbca390173dc0682f802cd30735fdb4dc32b62"}
+// session_id: c0521fc84f9a8034f98b1d6e1e08baf96c724507
+// account_id: 11489858
+
 const API_KEY = 'api_key=dab2c8f66df561816f3de5a1bdb51d3a';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL_Movie = BASE_URL + '/search/movie?'+API_KEY;
-
+const POST_WATCH = 'https://api.themoviedb.org/3/account/11489858/watchlist?api_key=dab2c8f66df561816f3de5a1bdb51d3a&session_id=db44e77af280715b2d5fd071072ede3d8bb78a87'
+const POST_WATCH_SERIES = 'https://api.themoviedb.org/3/account/11489858/watchlist/tv?api_key=dab2c8f66df561816f3de5a1bdb51d3a&session_id=db44e77af280715b2d5fd071072ede3d8bb78a87'
+const WISHLIST_URL = 'https://api.themoviedb.org/3/account/11489858/watchlist/movies?api_key=dab2c8f66df561816f3de5a1bdb51d3a&language=en-US&session_id=c0521fc84f9a8034f98b1d6e1e08baf96c724507&sort_by=created_at.desc&page=1'
 
 const genres = [
     {
@@ -92,6 +99,28 @@ const search = document.getElementById('searchHeader');
 const tagsEl = document.getElementById('tags')
 const genreEl = document.getElementsByClassName('movie_genres')
 
+
+const wishArr = [] 
+async function getWishlist(url) {
+    await fetch(url).then(res => res.json()).then(data => {
+        var b = data.results
+        b.forEach(wishMovie => {
+            var x = wishMovie.id
+            wishArr.push(x)
+        })
+    })
+}
+getWishlist(WISHLIST_URL)
+console.log(wishArr)
+console.log('wishArr[0]: ', wishArr[0])
+console.log('wishArr length: ', wishArr.length)
+wishArr.forEach(id => {
+    console.log(id)
+})
+console.log('wishArr: ', wishArr)
+console.log('wishArr[0]: ', wishArr[0])
+console.log('wishArr length: ', wishArr.length)
+
 var selectedGenre = []
 setGanre();
 function setGanre() {
@@ -147,11 +176,18 @@ function getMoviesPopular(url) {
 }
 
 
+var thumbsUpIcon = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402m5.726-20.583c-2.203 0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183 0-6.281 2.187-6.281 6.191 0 4.661 5.571 9.429 12 15.809 6.43-6.38 12-11.148 12-15.809 0-4.011-3.095-6.181-6.274-6.181"/></svg>`
+var wishListIcon = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="nonzero" clip-rule="nonzero"><path d="M5 0v24l7-6 7 6v-24h-14zm1 1h12v20.827l-6-5.144-6 5.144v-20.827z"/></svg>`
+var wishListIconFull = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 24l-6-5.269-6 5.269v-24h12v24z"/></svg>`
+
+
+
+
 function showMovies(data) {
     mainMovie.innerHTML = '';
     var i=0
     data.forEach(movie => {
-        const {title, poster_path, vote_average, overview,genre_ids} = movie;
+        const {title, poster_path, vote_average, overview,genre_ids, id} = movie;
         const movieEl = document.createElement('div');
         
         function setMovieGenre(genre_ids) {
@@ -172,11 +208,20 @@ function showMovies(data) {
             })
         }
 
+        var movieAdded= false
+
+        for (let i = 0; i < wishArr.length; i++) {
+            if (wishArr[i] == id) {
+                console.log('test',i)
+            }
+        }
 
         movieEl.classList.add('collection__item');
+        movieEl.dataset.id = id;
+        movieEl.dataset.movie = 'movie'+i
         movieEl.innerHTML = `
         <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580"}" alt="${title}">
-        <div class="collection__item-info">
+        <div class="collection__item-info" data-type="movie" data-id="${id}">
             <h3>${title}</h3>
             <span class="${getColor(vote_average)}">${vote_average}</span>
         </div>
@@ -184,6 +229,7 @@ function showMovies(data) {
             <h3>Overview</h3>
             ${overview}
             <div class="collection_genres-wrapper" id="movie${i}"></div>
+            <div class="fav-wish"><span class="add-icon add-fav" data-fav="${id}" data-type="movie">${thumbsUpIcon}<span class="add-tooltip fav-tooltip">Add to Favourites</span></span><span class="add-icon add-wish" data-wish="${id}" data-type="movie">${movieAdded? wishListIconFull: wishListIcon}<span class="add-tooltip wish-tooltip">Add to Wishlist</span></span></div>
         </div>
         
         `
@@ -192,6 +238,7 @@ function showMovies(data) {
         i++;
     });
 }
+
 
 function getColor(vote) {
     if (vote >= 8) {
@@ -205,6 +252,7 @@ function getColor(vote) {
     }
 }
 
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const searchTerm = search.value;
@@ -213,3 +261,37 @@ form.addEventListener('submit', (e) => {
         getMoviesPopular(searchURL_Movie+'&query='+ searchTerm)
     }
 })
+
+
+const addedToWish = []
+
+setTimeout(() => {
+function addToFavWish() {
+    var x = localStorage.getItem('moviesId')
+
+    const wishlist = document.querySelectorAll('.collection__item .add-wish')
+    var wishlistNum = wishlist.length
+    for (let i = 0; i < wishlistNum; i++) {
+        wishlist[i].addEventListener('click', () => {
+            var itemID = wishlist[i].dataset.wish
+            var itemType = wishlist[i].dataset.type
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", POST_WATCH, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            addedToWish.push(itemID)
+            localStorage.setItem("moviesId", JSON.stringify(addedToWish));
+            xhr.send(JSON.stringify({
+                media_type: `${itemType}`,
+                media_id: itemID,
+                watchlist: true
+            }));
+            wishlist[i].innerHTML = wishListIconFull
+            console.log('Item added to Watchlist')
+        })
+    }
+
+}
+addToFavWish()
+}, 200)
+
+
